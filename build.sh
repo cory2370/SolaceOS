40e0ba49f219bdc1a49f19635714ed78af4d5d6d
cory2370 <tung061741@stu.vinschool.edu.vn>
2025-07-26 19:42:46 +0000

    Added airootfs, calamares config, build script, blah blah blah

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -e
+
+#test comment test comment solaceos is the best linux distro
+mkdir -p /etc/artools/profiles/solaceos/airootfs
+rm -rf /etc/artools/profiles/solaceos/airootfs/*
+mkdir -p /etc/artools/profiles/solaceos/airootfs/etc/calamares
+cp -r airootfs/ /etc/artools/profiles/solaceos/airootfs/
+cp packages.x86_64 /etc/artools/profiles/solaceos/
+cp profiledef.sh /etc/artools/profiles/solaceos/
+cp -r calamares/ /etc/artools/profiles/solaceos/airootfs/etc/calamares
+buildiso -p solaceos
+
+#its pronounced "solace-sos" btw not "solace oh es"
+
+
+mkdir -p out
+mv /var/cache/isos/solaceos-*.iso out/
+echo "/$(ls out/) :DDDDDDD"
