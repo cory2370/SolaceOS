3fffe956856624d746691c3d466e523f966bc542
Ngo Quang Tung <tung061741@stu.vinschool.edu.vn>
2025-08-09 11:19:39 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
 
+WORKDIR="$HOME/solaceos-build"
+RUNTIME="$WORKDIR/runtime"
+OUTDIR="$WORKDIR/out"
+PROFILE="/etc/artools/profiles/solaceos"
+AIROOTFS_SRC="airootfs"
+CALAMARES_SRC="calamares"
+PKG_FILE="packages.x86_64"
+PROFILEDEF="profiledef.sh"
+
 #this is here to fix a bootfs error
 mkdir -p ~/solaceos-build/runtime
 mkdir -p ~/solaceos-build/runtime
 sudo mount --bind ~/solaceos-build/runtime /var/lib/artools/buildiso
-sudo buildiso -p solaceos -w ~/solaceos-build/workdir -o ~/solaceos-build/out
 
 set -e
 
@@ -16,7 +24,9 @@ cp -r airootfs/ /etc/artools/profiles/solaceos/airootfs/
 cp packages.x86_64 /etc/artools/profiles/solaceos/
 cp profiledef.sh /etc/artools/profiles/solaceos/
 cp -r calamares/ /etc/artools/profiles/solaceos/airootfs/etc/calamares
-sudo buildiso -p solaceos
+
+#2 buildiso commands, maybe its buildiso breaks in vm?? First buildiso is moved down here
+sudo buildiso -p solaceos -w ~/solaceos-build/workdir -o ~/solaceos-build/out
 
 #its pronounced "solace-sos" btw not "solace oh es"
 
